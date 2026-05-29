f3ccc865ac1597aa03c53905e2fb69cd3b9f6a42
absterr <absterr.dev@gmail.com>
2026-05-29 12:57:37 +0100

    remove vercel env check from server.ts

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -4,62 +4,58 @@ import { closeDB } from "./db";
 
 const PORT = env.API_PORT;
 
-// Use Bun.serve only in local or other server-based bun environments
-if (!process.env.VERCEL) {
-  console.log("Starting API server on a serverful environment...");
+// Bun.serve is not supported in some serverless environments
+const server = Bun.serve({
+  fetch: app.fetch,
+  port: PORT,
+});
 
-  const server = Bun.serve({
-    fetch: app.fetch,
-    port: PORT,
-  });
+console.log(`API server is running on port ${PORT}`);
 
-  console.log(`API server is running on port ${PORT}`);
+let isShuttingDown = false;
 
-  let isShuttingDown = false;
-
-  async function shutdown() {
-    if (isShuttingDown) {
-      return;
-    }
-
-    isShuttingDown = true;
-    console.info("> attempting graceful shutdown");
-    server.stop();
-    console.info("> HTTP server terminated");
-
-    try {
-      await closeDB();
-      console.info("> database connection closed");
-    } catch (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    process.exit(0);
+async function shutdown() {
+  if (isShuttingDown) {
+    return;
   }
 
-  // SERVER EVENT LISTENERS
+  isShuttingDown = true;
+  console.info("> attempting graceful shutdown");
+  server.stop();
+  console.info("> HTTP server terminated");
 
-  process.on("SIGINT", shutdown);
-  process.on("SIGTERM", shutdown);
-
-  process.on("unhandledRejection", async (err) => {
-    console.error("> unhandled rejection");
-
-    if (err instanceof Error) {
-      console.error(err.name, err.message);
-    } else {
-      console.error(err);
-    }
-
-    await shutdown();
+  try {
+    await closeDB();
+    console.info("> database connection closed");
+  } catch (err) {
+    console.error(err);
     process.exit(1);
-  });
+  }
 
-  process.on("uncaughtException", async (err) => {
-    console.error("> uncaught exception");
-    console.error(err.name, err.message);
-    await shutdown();
-    process.exit(1);
-  });
+  process.exit(0);
 }
+
+// SERVER EVENT LISTENERS
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+process.on("unhandledRejection", async (err) => {
+  console.error("> unhandled rejection");
+
+  if (err instanceof Error) {
+    console.error(err.name, err.message);
+  } else {
+    console.error(err);
+  }
+
+  await shutdown();
+  process.exit(1);
+});
+
+process.on("uncaughtException", async (err) => {
+  console.error("> uncaught exception");
+  console.error(err.name, err.message);
+  await shutdown();
+  process.exit(1);
+});
