507b319426d822c339f544d632422954b9f62e15
absterr <absterr.dev@gmail.com>
2026-06-04 12:53:27 +0100

    remove leads page components

--- apps/web/src/app/(main)/leads/page.tsx
+++ apps/web/src/app/(main)/leads/page.tsx
@@ -1,10 +1,7 @@
-import LeadsWrapper from "./LeadsWrapper";
-
 export default function Leads() {
   return (
     <div className="h-full flex flex-col p-2 sm:p-4">
       <h1 className="font-medium text-xl sm:text-2xl">Find Leads</h1>
-      <LeadsWrapper />
     </div>
   );
 }
