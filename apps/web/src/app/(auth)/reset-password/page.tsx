59cc17023406ac79e4809528adc2b7afd21062fb
absterr <enzsama.dev@gmail.com>
2026-06-14 02:22:25 +0100

    add reset-password page ui

---
+++ apps/web/src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordForm from "./ResetPasswordForm";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-foreground/5 dark:bg-background">
+      <ResetPasswordForm />
+    </div>
+  );
+}
