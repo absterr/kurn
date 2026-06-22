621d33bebf6d1d489330ed9464a64109540fa26b
absterr <enzsama.dev@gmail.com>
2026-06-22 23:09:55 +0100

    add lead queries GET endpoint

--- apps/api/src/routes/v1/leads/leads.v1.handlers.ts
+++ apps/api/src/routes/v1/leads/leads.v1.handlers.ts
@@ -1,10 +1,30 @@
-import type { Selectable } from "kysely";
+import { type Selectable, sql } from "kysely";
 import type { z } from "zod";
 import { makeDB } from "@/db/index.js";
 import type { Users } from "@/db/types.js";
 import type { leadsQuerySchema } from "./leads.v1.schema.js";
 
-export const createLeadsQuery = async (
+export const getLeadQueries = async (memberId: Selectable<Users>["id"]) => {
+  const leadQueries = await makeDB()
+    .selectFrom("leadQueries")
+    .leftJoin("leads", "leads.leadQueryId", "leadQueries.id")
+    .where("userId", "=", memberId)
+    .select([
+      "leadQueries.id",
+      "leadQueries.status",
+      "leadQueries.keyword",
+      "leadQueries.location",
+      "leadQueries.createdAt",
+      "leadQueries.updatedAt",
+    ])
+    .select(() => [sql<number>`count(leads.id)::int`.as("resultsCount")])
+    .groupBy("leadQueries.id")
+    .execute();
+
+  return leadQueries;
+};
+
+export const addLeadQuery = async (
   queries: z.infer<typeof leadsQuerySchema>,
   memberId: Selectable<Users>["id"],
 ) => {
--- apps/api/src/routes/v1/leads/leads.v1.routes.ts
+++ apps/api/src/routes/v1/leads/leads.v1.routes.ts
@@ -6,17 +6,27 @@ import {
   roleMiddleware,
 } from "@/lib/middleware.js";
 import { zValidate } from "@/utils/z-validate.js";
-import { createLeadsQuery } from "./leads.v1.handlers.js";
+import { addLeadQuery, getLeadQueries } from "./leads.v1.handlers.js";
 import { leadsQuerySchema } from "./leads.v1.schema.js";
 
 export const leadsV1Router = new Hono<AuthVariables>();
 leadsV1Router.use("*", authMiddleware, roleMiddleware("member"));
 
+leadsV1Router.get("/", async (ctx) => {
+  try {
+    const memberId = ctx.get("userId");
+    return ctx.json(await getLeadQueries(memberId), 200);
+  } catch (err) {
+    if (err instanceof HTTPException) throw err;
+    return ctx.json({ error: "Something went wrong" }, 500);
+  }
+});
+
 leadsV1Router.post("/", zValidate("json", leadsQuerySchema), async (ctx) => {
   try {
     const valid = ctx.req.valid("json");
     const memberId = ctx.get("userId");
-    return ctx.json(await createLeadsQuery(valid, memberId), 201);
+    return ctx.json(await addLeadQuery(valid, memberId), 201);
   } catch (err) {
     if (err instanceof HTTPException) throw err;
     return ctx.json({ error: "Something went wrong" }, 500);
